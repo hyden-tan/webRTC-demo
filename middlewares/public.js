dc71e6275224088fed48445f5d879688b101966f
yunnan <yunnan@indata.cc>
2020-02-10 21:16:51 +0800

    Revert "fix: j兼容服务器端低版本node"
    This reverts commit 65ff2918990d312b7c4c2db05422d7d5cbd6f2ef.

--- middlewares/public.js
+++ middlewares/public.js
@@ -4,15 +4,15 @@ const path = require('path');
 const fsPromises = fs.promises;
 const root = process.cwd();
 
-module.exports = (publicPath) => (ctx, next) => {
+module.exports = (publicPath) => async (ctx, next) => {
     if (ctx.path.indexOf(publicPath) === 1) {
         if (/.css$/.test(ctx.path)) {
             ctx.type = 'text/css';
         }
 
 
-        ctx.body = fsPromises.readFile(path.join(root, ctx.path), { encoding: 'utf8' });
+        ctx.body = await fsPromises.readFile(path.join(root, ctx.path), { encoding: 'utf8' });
     }
 
-    next();
+    await next();
 }
